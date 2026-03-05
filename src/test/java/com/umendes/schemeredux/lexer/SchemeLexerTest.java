f8c6eb97a42644be12ee995985ccea12dfecd57f
Uche Mendes <156598397+UCMendes@users.noreply.github.com>
2026-03-05 22:56:14 +0000

    First test made, as test

--- src/test/java/com/umendes/schemeredux/lexer/SchemeLexerTest.java
+++ src/test/java/com/umendes/schemeredux/lexer/SchemeLexerTest.java
@@ -1,13 +1,17 @@
 package com.umendes.schemeredux.lexer;
 
+import org.jparsec.Tokens;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchemeLexerTest {
 
+    SchemeLexer testLexer = new SchemeLexer();
+
     @Test
-    void start() {
-        assertEquals(4, 3 * 2);
+    void SCA_DATUM_COMMENT_PREFIX() {
+        Tokens.Fragment test_fragment = (Tokens.Fragment) testLexer.s_datum_comment_prefix.parse("#;");
+        assertEquals("#;", test_fragment.text());
     }
 }
